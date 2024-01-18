3c18e2a09b72b920554a9e411c7b4bfdd71e24b8
genocider34 <alonzoball07@gmail.com>
2024-01-18 18:57:42 +0800

    Need Refactoring and Small Fixes

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
-const player1Score = document.getElementById("player1Score");
-const player2Score = document.getElementById("player2Score");
-const player1Btn = document.querySelector(".player1Btn");
-const player2Btn = document.querySelector(".player2Btn");
-const resetBtn = document.querySelector(".resetBtn");
-const goalSelect = document.querySelector("#score-goal");
-let currentPlayer1Score = 0;
-let currentPlayer2Score = 0;
-let finalGoal = 0;
-goalSelect.addEventListener("change", () => {
-  finalGoal = goalSelect.value;
-  if (currentPlayer1Score >= finalGoal) {
-    console.log("Player 1 Win");
-    // player1Score.classList.add("win-active");
-    // player2Score.classList.add("lose-active");
+const p1Display = document.querySelector(".p1-display");
+const p2Display = document.querySelector(".p2-display");
+const p1Button = document.querySelector(".p1-btn");
+const p2Button = document.querySelector(".p2-btn");
+const resetButton = document.querySelector(".reset-btn");
+const winningScoreSelect = document.querySelector("#play-to");
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 0;
+let isGameOver = false;
+
+winningScoreSelect.addEventListener("change", () => {
+  winningScore = parseInt(this.value);
+  reset();
+});
+p1Button.addEventListener("click", () => {
+  if (!isGameOver) {
+    p1Score++;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
+    }
+    p1Display.textContent = p1Score;
   }
 });
 
-player1Btn.addEventListener("click", () => {
-  currentPlayer1Score++;
-  player1Score.textContent = currentPlayer1Score;
+p2Button.addEventListener("click", () => {
+  if (!isGameOver) {
+    p2Score++;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+      p2Display.classList.add("winner");
+      p1Display.classList.add("loser");
+    }
+    p2Display.textContent = p2Score;
+  }
 });
 
-player2Btn.addEventListener("click", () => {
-  currentPlayer2Score++;
-  player2Score.textContent = currentPlayer2Score;
-});
+resetButton.addEventListener("click", reset);
 
-resetBtn.addEventListener("click", () => {
-  player1Score.textContent = "0";
-  player2Score.textContent = "0";
-});
+function reset() {
+  isGameOver = false;
+  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("winner", "loser");
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+}
